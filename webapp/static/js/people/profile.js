a2d1af124e54a307910ce25cb54e6a61ac3b1658
jazz4web <webcodej@mail.ru>
2026-05-27 11:36:13 +0500

    Activate some buttons in user profile

--- webapp/static/js/people/profile.js
+++ webapp/static/js/people/profile.js
@@ -42,5 +42,89 @@ $(function() {
     },
     dataType: 'json'
   });
+  if (cu === username ) {
+    $('body').on('click', '#cancel-description', function() {
+      $(this).blur();
+      $(this).parents('#description-e').slideUp('slow');
+      let description = $('#description-block');
+      description.slideDown('slow', function() { scrollPanel(description); });
+    });
+    $('body').on('click', '#fix-description', function() {
+      $(this).blur();
+      let emf = $('#changeemf');
+      if (!emf.is(':hidden')) emf.slideUp('slow');
+      let pwdf = $('#changepwdf');
+      if (!pwdf.is(':hidden')) pwdf.slideUp('slow');
+      let avaf = $('#changeavaf');
+      if (!avaf.is(':hidden')) avaf.slideUp('slow');
+      $(this).parents('#description-block').slideUp('slow');
+      let editor = $('#description-e');
+      editor.slideDown('slow', function() { scrollPanel(editor); });
+      $('#description-editor').focus();
+    });
+    $('body').on('click', '#emchange', function() {
+      $(this).blur();
+      let em = $('#changeemf');
+      if (em.is(':hidden')) {
+        $('#changeavaf').slideUp('slow');
+        $('#changepwdf').slideUp('slow');
+        let de = $('#description-e');
+        if (!de.is(':hidden')) {
+          de.slideUp('slow');
+          $('#description-block').slideDown('slow');
+        }
+        em.slideDown('slow', function() {
+          scrollPanel(em);
+          checkPC(860);
+        });
+      } else {
+        em.slideUp('slow', function() {
+          checkPC(860);
+        });
+      }
+    });
+    $('body').on('click', '#changepwd', function() {
+      $(this).blur();
+      let pwd = $('#changepwdf');
+      if (pwd.is(':hidden')) {
+        $('#changeavaf').slideUp('slow');
+        $('#changeemf').slideUp('slow');
+        let de = $('#description-e');
+        if (!de.is(':hidden')) {
+          de.slideUp('slow');
+          $('#description-block').slideDown('slow');
+        }
+        pwd.slideDown('slow', function() {
+          scrollPanel(pwd);
+          checkPC(860);
+        });
+      } else {
+        pwd.slideUp('slow', function() {
+          checkPC(860);
+        });
+      }
+    });
+    $('body').on('click', '#changeava', function() {
+      $(this).blur();
+      let ava = $('#changeavaf');
+      if (ava.is(':hidden')) {
+        $('#changepwdf').slideUp('slow');
+        $('#changeemf').slideUp('slow');
+        let de = $('#description-e');
+        if (!de.is(':hidden')) {
+          de.slideUp('slow');
+          $('#description-block').slideDown('slow');
+        }
+        ava.slideDown('slow', function() {
+          scrollPanel(ava);
+          checkPC(860);
+        });
+      } else {
+        ava.slideUp('slow', function() {
+          checkPC(860);
+        });
+      }
+    });
+  }
   checkPC(860);
 });
