27607057587042bc4c1370977ad5a9c03d22c2dc
jazz4web <webcodej@mail.ru>
2026-08-01 08:50:42 +0500

    Show labels in blog

--- webapp/api/blogs.py
+++ webapp/api/blogs.py
@@ -6,7 +6,51 @@ from ..auth.cu import checkcu
 from ..common.aparsers import parse_page
 from ..common.pg import get_conn
 from ..drafts.attri import status
-from .pg import check_last, select_authored, select_authors
+from .pg import check_last, select_authored, select_authors, select_l_blog
+
+
+class LBlog(HTTPEndpoint):
+    async def get(self, request):
+        res = {'cu': None}
+        token = request.headers.get('x-auth-sestee')
+        if token is None:
+            raise HTTPException(403)
+        conn = await get_conn(request.app.config)
+        cu = await checkcu(request, conn, token)
+        res['cu'] = cu
+        username = request.query_params.get('username')
+        label = request.query_params.get('label')
+        author = await conn.fetchval(
+            'SELECT id FROM users WHERE username = $1', username)
+        if author is None:
+            res['message'] = f'{username} среди авторов не обнаружен.'
+            await conn.close()
+            return JSONResponse(res)
+        page = await parse_page(request)
+        last = await check_last(
+            conn, page,
+            request.app.config.get('ARTS_PER_PAGE', cast=int, default=3),
+            ''' SELECT count(*) FROM articles, labels, als
+                  WHERE articles.author_id = $1
+                    AND articles.id = als.article_id
+                    AND labels.label = $2
+                    AND labels.id = als.label_id
+                    AND articles.state IN ($3, $4, $5)''',
+            author, label, status.pub, status.priv, status.ffo)
+        if page > last:
+            res['message'] = f'Всего известно страниц: {last}.'
+            await conn.close()
+            return JSONResponse(res)
+        res['pagination'] = dict()
+        await select_l_blog(
+            request, conn, res['pagination'],
+            author, label, page,
+            request.app.config.get('ARTS_PER_PAGE', cast=int, default=3), last)
+        if res['pagination']:
+            if res['pagination']['next'] or res['pagination']['prev']:
+                res['pv'] = True
+        await conn.close()
+        return JSONResponse(res)
 
 
 class Blog(HTTPEndpoint):
--- webapp/api/pg.py
+++ webapp/api/pg.py
@@ -16,6 +16,25 @@ from .parse import parse_art_query, parse_arts_query
 from .slugs import check_max, make, parse_match
 
 
+async def select_l_blog(
+        request, conn, target, uid, label, page, per_page, last):
+    query = await conn.fetch(
+        '''SELECT a.id, a.title, a.slug, a.suffix, a.summary, a.published,
+                  a.edited, a.state, a.commented, a.viewed, users.username
+             FROM articles AS a, users, labels, als
+             WHERE a.author_id = users.id
+               AND a.author_id = $1
+               AND a.id = als.article_id
+               AND labels.label = $2
+               AND labels.id = als.label_id
+               AND a.state IN ($3, $4, $5)
+             ORDER BY a.published ASC LIMIT $6 OFFSET $7''',
+        uid, label, status.pub, status.priv, status.ffo,
+        per_page, per_page*(page-1))
+    if query:
+        await parse_arts_query(request, conn, query, target, page, last)
+
+
 async def select_authored(request, conn, target, uid, page, per_page, last):
     query = await conn.fetch(
         '''SELECT a.id, a.title, a.slug, a.suffix, a.summary, a.published,
