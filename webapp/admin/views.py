179019aafc7ee2d9b80572edfd33cc422a35831d
jazz4web <webcodej@mail.ru>
2026-09-16 05:24:50 +0500

    Fix some issues

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -22,7 +22,7 @@ async def show_log(request):
         else:
             l = '/var/log/nginx/access.log.1'
         if os.path.exists(l):
-            response = FileResponse(l)
+            response = FileResponse(path=l, media_type='text/plain')
         else:
             a = 'Файл не существует.\n'
             m = 'Убедитесь, что вы используете Nginx.'
